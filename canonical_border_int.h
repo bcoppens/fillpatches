c017da5b9e37eb1cb5c425fdb2e5aa5471080e38
Bart Coppens <kde@bartcoppens.be>
2008-07-08 17:09:05 +0200

    Extend and fix the 'arbitrary' size canonical border code
    darcs-hash:20080708150905-3bc9d-e95ca1639e45620ea05ebc964117eb23123a554f

--- canonical_border_int.h
+++ canonical_border_int.h
@@ -16,7 +16,7 @@
 struct CanonicalBorderInt {
     CanonicalBaseType content[CanonicalSize]; // most significant content left...
     void clear() {
-        memset(this, 0, sizeof(this));
+        memset(this, 0, sizeof(*this));
     }
 
     CanonicalBorderInt() {
@@ -68,14 +68,65 @@ struct CanonicalBorderInt {
             // Partial shifts:
             CanonicalBaseType sleft = shift % BaseBitSize;
             CanonicalBaseType sright = BaseBitSize - sleft;
-            int idx;
-            for (int i = 0; (i < CanonicalSize) && ((i + (shift / BaseBitSize)) < CanonicalSize); i++) {
+            int idx, i;
+            for (i = 0; (i < CanonicalSize) && ((i + (shift / BaseBitSize)) < CanonicalSize); i++) {
                 idx = i + (shift / BaseBitSize);
-                c.content[i] = (content[idx] << sleft) | (content[idx+1] >> sright);
+                if (idx + 1 < CanonicalSize)
+                    c.content[i] = (content[idx] << sleft) | (content[idx+1] >> sright);
+                else // Final round:
+                    c.content[i] = (content[idx] << sleft);
             }
         }
 
         return c;
+    }
+
+    CanonicalBorderInt operator>>(const CanonicalBorderInt& rhs) const {
+        // ### rhs is actually a BaseType!!!!!
+        CanonicalBorderInt c;
+        CanonicalBaseType shift = rhs.content[CanonicalSize - 1];
+        if (shift % BaseBitSize == 0) {
+            // Whole shift:
+            for (int i = CanonicalSize - 1; (i >= 0) && (signed(i - (shift / BaseBitSize)) >= 0); i--) {
+                c.content[i] = content[i - (shift / BaseBitSize)];
+            }
+        } else {
+            // [0001][1111][1110] >> 2
+            // Partial shifts:
+            CanonicalBaseType sright = shift % BaseBitSize;
+            CanonicalBaseType sleft = BaseBitSize - sright;
+            int idx, i;
+            for (i = CanonicalSize - 1; i >= 0 && (signed(i - (shift / BaseBitSize)) >= 0); i--) {
+                idx = i - (shift / BaseBitSize);
+                if (idx - 1 >= 0)
+                    c.content[i] = (content[idx] >> sright) | (content[idx-1] << sleft);
+                else // Final round:
+                    c.content[i] = (content[idx] >> sright);
+            }
+        }
+
+        return c;
+    }
+
+    bool operator==(const CanonicalBorderInt& rhs) const {
+        for (int i = 0; i < CanonicalSize; i++)
+            if (content[i] != rhs.content[i])
+                return false;
+        return true;
+    }
+
+    bool operator!=(const CanonicalBorderInt& rhs) const {
+        for (int i = 0; i < CanonicalSize; i++)
+            if (content[i] == rhs.content[i])
+                return false;
+        return true;
+    }
+
+    bool operator<(const CanonicalBorderInt& rhs) const {
+        for (int i = 0; i < CanonicalSize; i++)
+            if (content[i] < rhs.content[i])
+                return true;
+        return false;
     }
 
     // Slow...
@@ -83,6 +134,15 @@ struct CanonicalBorderInt {
         *this = *this << rhs;
         return *this;
     }
+
+    operator off_t() const {
+        // Call only if offset fits in basetype!
+        assert(sizeof(off_t) <= sizeof(CanonicalBaseType));
+        // Only if this can be converted...
+        // ### Check
+        return content[CanonicalSize - 1];
+    }
 };
+
 
 #endif // CANONICAL_BORDER_INT
--- canonical_border_test.cc
+++ canonical_border_test.cc
@@ -14,22 +14,48 @@ void testShl() {
     for (unsigned int i = 0; i < 0xffff; i++) {
         CanonicalBorderInt J = CanonicalBorderInt(i & 0xff);
         assert(J.content[3] == (i & 0xff));
+        //CanonicalBorderInt S = CanonicalBorderInt(i >> 8) << CanonicalBorderInt(8);
+        //J |= S;
         J |= CanonicalBorderInt(i >> 8) << CanonicalBorderInt(8);
         assert(J.content[3] == (i & 0xff));
         assert(J.content[2] == ((i >> 8) & 0xff));
 
-        for (unsigned int k = 0; k < 32; k++) {
-            unsigned int j = i << k;
-            J <<= CanonicalBorderInt(k);
-            assert((0xff & (j)) == J.content[3]);
-            assert((0xff & (j>>8)) == J.content[2]);
-            assert((0xff & (j>>16)) == J.content[1]);
-            assert((0xff & (j>>24)) == J.content[0]);
+        cout << i << endl;
+        for (unsigned int k = 0; k < 64; k++) {
+            unsigned int j =(unsigned long)(i) << k;
+            CanonicalBorderInt JJ = J;
+            JJ <<= CanonicalBorderInt(k);
+            assert((0xff & (j)) == JJ.content[3]);
+            assert((0xff & (j>>8)) == JJ.content[2]);
+            assert((0xff & (j>>16)) == JJ.content[1]);
+            assert((0xff & (j>>24)) == JJ.content[0]);
         }
     }
 }
 
+void testShr() {
+    for (unsigned int i = 0; i < 0xffff; i++) {
+        CanonicalBorderInt J = CanonicalBorderInt(i & 0xff);
+        assert(J.content[3] == (i & 0xff));
+        //CanonicalBorderInt S = CanonicalBorderInt(i >> 8) << CanonicalBorderInt(8);
+        //J |= S;
+        J |= CanonicalBorderInt(i >> 8) << CanonicalBorderInt(8);
+        assert(J.content[3] == (i & 0xff));
+        assert(J.content[2] == ((i >> 8) & 0xff));
+
+        cout << i << endl;
+        for (unsigned int k = 0; k < 64; k++) {
+            unsigned int j = (unsigned long)(i) >> k;
+            CanonicalBorderInt JJ = J >> CanonicalBorderInt(k);
+            assert((0xff & (j)) == JJ.content[3]);
+            assert((0xff & (j>>8)) == JJ.content[2]);
+            assert((0xff & (j>>16)) == JJ.content[1]);
+            assert((0xff & (j>>24)) == JJ.content[0]);
+        }
+    }
+}
 
 int main() {
     testShl();
+    testShr();
 }
